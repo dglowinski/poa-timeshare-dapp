ccb7c05c90b7ecefff3be71b7052790df09a662e
Darek <derek@2gether.global>
2018-08-12 13:55:08 +0200

    add ropsten deploy

--- truffle.js
+++ truffle.js
@@ -1,3 +1,7 @@
+require('dotenv').config()
+
+const HDWalletProvider = require('truffle-hdwallet-provider')
+
 module.exports = {
   solc: {
     optimizer: {
@@ -11,6 +15,19 @@ module.exports = {
       port: 8545,
       network_id: '*', // Match any network id
       gas: 4712388
+    },
+    ropsten: {
+      provider: function() {
+        return new HDWalletProvider(
+          process.env.MNEMONIC,
+          'https://ropsten.infura.io/v3/' + process.env.INFURA,
+          0,
+          10
+        )
+      },
+      network_id: 3,
+      gas: 4712388,
+      gasPrice: 20000000000
     }
   }
 }
